0a05596e7f056cb103650855f74e364a0b2ab7a0
bryantobando2425-dev <bryantobando2425@gmail.com>
2026-04-10 03:07:28 -0500

    Delete db directory

--- db/src/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./players";
-export * from "./runs";
-export * from "./run_events";
-export * from "./legacy_echoes";
-export * from "./moments";
-export * from "./achievements";
-export * from "./world_maps";
